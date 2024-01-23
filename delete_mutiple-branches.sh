96377ea43bc7ac1eb6aa7c2d1b8aee52e71ade66
sidharth <sidharthvijayakumar7@gmail.com>
2024-01-23 15:04:09 +0530

    made change to delete branches with common pattern

---
+++ delete_mutiple-branches.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Get a list of remote branches matching the regex pattern
+matching_branches=($(git ls-remote --heads | grep -E "/feature/.*" | awk -F'/' '{print $4}'))
+
+# Loop through the matching branches and delete each one remotely
+for branch in "${matching_branches[@]}"
+do
+  # Delete remotely
+  git push --delete origin "$branch"
+done
